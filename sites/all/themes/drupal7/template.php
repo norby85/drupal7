510ef35fc095bd557b44be402f7a5cdefaea83c9
norby85 <moracsek@gmail.com>
2018-09-07 20:22:36 +0200

    Drupal template probe

--- sites/all/themes/drupal7/template.php
+++ sites/all/themes/drupal7/template.php
@@ -1,1 +1,19 @@
 <?php
+
+function drupal7_preprocess_page(&$variables) {
+    $variables['drupal7_subheading'] = theme_get_setting('drupal7_subheading');
+
+    if (isset($variables['node'])) {
+        if ($variables['node']->type == 'page') {
+            drupal_add_css(drupal_get_path('theme','drupal7')."/style.css");
+        }
+    }
+
+    if (isset($variables['node'])) {
+        if ($variables['node']->type == 'product') {
+            drupal_add_css(drupal_get_path('theme','drupal7')."/drupal7.css.less");
+            drupal_add_css(drupal_get_path('theme','drupal7')."/drupal7-grid.css.less");
+        }
+    }
+
+}
